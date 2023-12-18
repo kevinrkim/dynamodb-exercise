78096c46b312f64d40fb809bd1c16279cee742dd
Kevin Kim <github@kevinckr.com>
2023-12-18 13:16:45 +0100

    Initial commit

---
+++ test/dynamodb-exercise.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamodbExercise from '../lib/dynamodb-exercise-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamodb-exercise-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamodbExercise.DynamodbExerciseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
