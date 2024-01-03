8c10e641981e17e07e216f22273eaf46b7d62b4d
Kevin Kim <github@kevinckr.com>
2024-01-03 17:36:05 +0100

    remove lambda directory to restart crud functions

--- lib/dynamodb-exercise-stack.ts
+++ lib/dynamodb-exercise-stack.ts
@@ -8,30 +8,6 @@ export class DynamodbExerciseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define DynamoDB table
-    const table = new dynamodb.Table(this, 'MusicDB', {
-      partitionKey: {
-        name: 'id',
-        type: dynamodb.AttributeType.STRING,
-      },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
-    });
-
-    // Define lambda functions
-    const readLambda = new lambda.Function(this, 'ReadFunction', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: 'read.handler',
-      code: lambda.Code.fromAsset('lambda'),
-      environment: {
-        TABLE_NAME: 'MusicDB',
-      }
-    });
-
-    table.grantReadWriteData(readLambda);
-
-    // Define API Gateway
-    const api = new apigateway.RestApi(this, 'MusicAPI');
-    const items = api.root.addResource('items');
-    items.addMethod('GET', new apigateway.LambdaIntegration(readLambda));
+   
   }
 }
